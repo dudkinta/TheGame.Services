6ce02f17f5b5c05961ea8a9a28cf7b8ec1c4f056
Dudkin Timofey <dudkinta@gmail.com>
2024-08-03 15:00:56 +0300

    fix anry model

--- StatisticDbContext/Models/ArmyModel.cs
+++ StatisticDbContext/Models/ArmyModel.cs
@@ -1,10 +1,13 @@
-﻿namespace StatisticDbContext.Models
+﻿using System.Text.Json.Serialization;
+
+namespace StatisticDbContext.Models
 {
     public class ArmyModel
     {
         public int id { get; set; }
         public int user_id { get; set; }
         public int barrack_id { get; set; }
+        [JsonIgnore]
         public BarrackModel? barrack { get; set; }
         public ICollection<InventoryModel>? equip { get; set; }
         public int useType { get; set; }
