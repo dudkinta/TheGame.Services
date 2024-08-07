274133f6bc48da32345419b551a2233029c4558b
Dudkin Timofey <dudkinta@gmail.com>
2024-08-07 22:02:00 +0300

    add item up/down

---
+++ ExchangeData/Models/ChangeItemModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ExchangeData.Models
+{
+    public class ChangeItemModel
+    {
+        public int item_id { get; set; }
+        public int army_id { get; set; }
+    }
+}
--- StatisticService/Controllers/ItemController.cs
+++ StatisticService/Controllers/ItemController.cs
@@ -1,4 +1,5 @@
 ﻿using CommonLibs;
+using ExchangeData.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -23,13 +24,63 @@ namespace StatisticService.Controllers
 
         [Authorize(AuthenticationSchemes = "User", Policy = "UserPolicy")]
         [HttpGet("getitems")]
-        [Obsolete]
         public async Task<IActionResult> GetItems(string itemType, CancellationToken cancellationToken)
         {
             try
             {
                 var userId = _userService.GetUserId(User.Claims);
-                var inventory = await _context.Inventory.Include(_ => _.item).Where(_ => _.user_id == userId && _.item!.type == itemType).ToListAsync();
+                var inventory = await _context.Inventory.Include(_ => _.item).Where(_ => _.user_id == userId && _.item!.type == itemType).ToListAsync(cancellationToken);
+                return Ok(inventory);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [Authorize(AuthenticationSchemes = "User", Policy = "UserPolicy")]
+        [HttpPost("up")]
+        public async Task<IActionResult> UpItem(ChangeItemModel changeItem, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var userId = _userService.GetUserId(User.Claims);
+                var invItem = await _context.Inventory.Include(_ => _.item).FirstOrDefaultAsync(_ => _.user_id == userId && _.id == changeItem.item_id, cancellationToken);
+                if (invItem == null)
+                    return BadRequest("Not Item NotFound");
+
+                var currentItem = await _context.Inventory.Include(_ => _.item).FirstOrDefaultAsync(_ => _.user_id == userId && _.army_id == changeItem.army_id && _.item!.type == invItem.item!.type, cancellationToken);
+                if (currentItem != null)
+                {
+                    currentItem.army_id = null;
+                }
+                invItem.army_id = changeItem.army_id;
+                await _context.SaveAsync(cancellationToken);
+                var inventory = await _context.Inventory.Include(_ => _.item).Where(_ => _.user_id == userId && _.item!.type == invItem.item!.type).ToListAsync(cancellationToken);
+                return Ok(inventory);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [Authorize(AuthenticationSchemes = "User", Policy = "UserPolicy")]
+        [HttpPost("down")]
+        public async Task<IActionResult> DownItem(ChangeItemModel changeItem, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var userId = _userService.GetUserId(User.Claims);
+                var invItem = await _context.Inventory.FirstOrDefaultAsync(_ => _.user_id == userId && _.id == changeItem.item_id, cancellationToken);
+                if (invItem == null)
+                    return BadRequest("Not Item NotFound");
+
+                invItem.army_id = null;
+                await _context.SaveAsync(cancellationToken);
+                var inventory = await _context.Inventory.Include(_ => _.item).Where(_ => _.user_id == userId && _.item!.type == invItem.item!.type).ToListAsync(cancellationToken);
                 return Ok(inventory);
             }
             catch (Exception ex)
